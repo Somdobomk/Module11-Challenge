79a15b9a671d1ac2a930f83949196f122b1f550e
somdobomk <somdobomk@gmail.com>
2021-10-29 00:02:25 -0500

    fix htmlRouter/index.js router.get

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const fs = require('fs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
